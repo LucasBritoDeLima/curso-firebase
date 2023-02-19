4426d4ffd1d995d241ccd7567cfbb74c49f3b83e
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-02-19 19:52:00 -0300

    Insert Update Delete

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react';
 import { setStatusBarBackgroundColor, StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import firebase from './src/firebaseConnection';
 
 
 export default function App() {
 
   const [nome, setNome] = useState('Carregando...');
-  const [idade, setIdade] = useState('');
+  const [cargo, setCargo] = useState('');
 
   useEffect(()=> {
     async function dados(){
@@ -32,12 +32,62 @@ export default function App() {
     dados();
   }, []);
 
+  async function cadastrar(){
+    if(nome !== '' && cargo !== ''){
+      let usuarios = await firebase.database().ref('usuarios');
+      let chave = usuarios.push().key;
+
+      usuarios.child(chave).set({
+        nome: nome,
+        cargo: cargo
+      });
+
+      alert('Cadastrado com sucesso!');
+      setCargo('');
+      setNome('');
+    }
+
+  }
+
   return (
-    <View style={{marginTop: 35}}>
-      
-      <StatusBar backgroundColor='#000'/>
-      <Text style={{ fontSize: 25}}>Olá {nome}</Text>
-      <Text style={{ fontSize: 25}}>Idade {idade}</Text>
+    <View style={styles.container}>
+      <Text style={styles.texto}>Nome</Text>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid="transparent"
+        onChangeText={(texto) => setNome(texto)}
+        value={nome}
+      />
+      <Text style={styles.texto}>Cargo</Text>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid="transparent"
+        onChangeText={(texto) => setCargo(texto)}
+        value={cargo}
+      />
+
+      <Button
+        title="Novo Funcionário"
+        onPress={cadastrar}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    margin: 10
+  },
+  texto:{
+    fontSize: 20
+  },
+  input:{
+    marginBottom: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#121212',
+    height: 45,
+    fontSize: 17
+  }
+})
